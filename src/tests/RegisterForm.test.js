6322d3eb76341f1ac0f578c3a17ddaaae266a55c
Elelia <llucas@ensup.eu>
2024-02-11 19:40:50 +0100

    update

--- src/tests/RegisterForm.test.js
+++ src/tests/RegisterForm.test.js
@@ -1,4 +1,5 @@
 import { render, screen, fireEvent, waitFor } from '@testing-library/react';
+import { toast } from 'react-toastify';
 
 import React from 'react';
 import '@testing-library/jest-dom';
@@ -7,6 +8,7 @@ import RegisterForm from '../components/RegisterForm';
 const dayjs = require('dayjs');
 
 describe('Tests d\'intégration pour le formulaire', () => {
+  // Test de rendu du formulaire
   test('Vérification des éléments présents dans le formulaire', async () => {
     render(<RegisterForm />);
   
@@ -35,7 +37,8 @@ describe('Tests d\'intégration pour le formulaire', () => {
     expect(submitButton).toBeInTheDocument();
   });
 
-  test("submit button is disabled initially", () => {
+  // Test de désactivation du bouton d'inscription
+  test("Le bouton d'inscription est désactivé à l'ouverture du composant", () => {
     render(<RegisterForm />);
   
     const submitButton = screen.getByRole("button", { name: /Inscription/ });
@@ -43,6 +46,115 @@ describe('Tests d\'intégration pour le formulaire', () => {
     expect(submitButton).toBeDisabled();
   });
 
+  // Test de désactivation du bouton d'inscription si un champ est vide
+  test("Le bouton d'inscription est désactivé si le nom est vide", () => {
+    render(<RegisterForm />);
+  
+    const submitButton = screen.getByRole("button", { name: /Inscription/ });
+    expect(submitButton).toBeDisabled();
+  
+    fireEvent.change(screen.getByLabelText(/Prénom/), { target: { value: 'John' } });
+    fireEvent.change(screen.getByLabelText(/Adresse mail/), { target: { value: 'johndoe@gmail.com' } });
+    fireEvent.change(screen.getByLabelText(/Mot de passe/), { target: { value: 'Jk0nl!56HJ' } });
+    fireEvent.change(screen.getByLabelText(/Date de naissance/), { target: { value: dayjs('01-01-2000') } });
+    fireEvent.change(screen.getByLabelText(/Ville/), { target: { value: 'New York' } });
+    fireEvent.change(screen.getByLabelText(/Code postal/), { target: { value: '10001' } });
+    fireEvent.change(screen.getByLabelText(/Nom/), {target: { value: "" },});
+  
+    expect(submitButton).toBeDisabled();
+  });
+
+  // Test de désactivation du bouton d'inscription si un champ est vide
+  test("Le bouton d'inscription est désactivé si le prénom est vide", () => {
+    render(<RegisterForm />);
+  
+    const submitButton = screen.getByRole("button", { name: /Inscription/ });
+    expect(submitButton).toBeDisabled();
+  
+    fireEvent.change(screen.getByLabelText(/Nom/), { target: { value: 'Doe' } });
+    fireEvent.change(screen.getByLabelText(/Adresse mail/), { target: { value: 'johndoe@gmail.com' } });
+    fireEvent.change(screen.getByLabelText(/Mot de passe/), { target: { value: 'Jk0nl!56HJ' } });
+    fireEvent.change(screen.getByLabelText(/Date de naissance/), { target: { value: dayjs('01-01-2000') } });
+    fireEvent.change(screen.getByLabelText(/Ville/), { target: { value: 'New York' } });
+    fireEvent.change(screen.getByLabelText(/Code postal/), { target: { value: '10001' } });
+    fireEvent.change(screen.getByLabelText(/Prénom/), {target: { value: "" },});
+  
+    expect(submitButton).toBeDisabled();
+  });
+
+  // Test de désactivation du bouton d'inscription si un champ est vide
+  test("Le bouton d'inscription est désactivé si l'adresse mail est vide", () => {
+    render(<RegisterForm />);
+  
+    const submitButton = screen.getByRole("button", { name: /Inscription/ });
+    expect(submitButton).toBeDisabled();
+  
+    fireEvent.change(screen.getByLabelText(/Nom/), { target: { value: 'Doe' } });
+    fireEvent.change(screen.getByLabelText(/Prénom/), { target: { value: 'John' } });
+    fireEvent.change(screen.getByLabelText(/Mot de passe/), { target: { value: 'Jk0nl!56HJ' } });
+    fireEvent.change(screen.getByLabelText(/Date de naissance/), { target: { value: dayjs('01-01-2000') } });
+    fireEvent.change(screen.getByLabelText(/Ville/), { target: { value: 'New York' } });
+    fireEvent.change(screen.getByLabelText(/Code postal/), { target: { value: '10001' } });
+    fireEvent.change(screen.getByLabelText(/Adresse mail/), {target: { value: "" },});
+  
+    expect(submitButton).toBeDisabled();
+  });
+
+  // Test de désactivation du bouton d'inscription si un champ est vide
+  test("Le bouton d'inscription est désactivé si le mot de passe est vide", () => {
+    render(<RegisterForm />);
+  
+    const submitButton = screen.getByRole("button", { name: /Inscription/ });
+    expect(submitButton).toBeDisabled();
+  
+    fireEvent.change(screen.getByLabelText(/Nom/), { target: { value: 'Doe' } });
+    fireEvent.change(screen.getByLabelText(/Prénom/), { target: { value: 'John' } });
+    fireEvent.change(screen.getByLabelText(/Adresse mail/), { target: { value: 'johndoe@gmail.com' } });
+    fireEvent.change(screen.getByLabelText(/Date de naissance/), { target: { value: dayjs('01-01-2000') } });
+    fireEvent.change(screen.getByLabelText(/Ville/), { target: { value: 'New York' } });
+    fireEvent.change(screen.getByLabelText(/Code postal/), { target: { value: '10001' } });
+    fireEvent.change(screen.getByLabelText(/Mot de passe/), {target: { value: "" },});
+  
+    expect(submitButton).toBeDisabled();
+  });
+
+  // Test de désactivation du bouton d'inscription si un champ est vide
+  test("Le bouton d'inscription est désactivé si la ville est vide", () => {
+    render(<RegisterForm />);
+  
+    const submitButton = screen.getByRole("button", { name: /Inscription/ });
+    expect(submitButton).toBeDisabled();
+  
+    fireEvent.change(screen.getByLabelText(/Nom/), { target: { value: 'Doe' } });
+    fireEvent.change(screen.getByLabelText(/Prénom/), { target: { value: 'John' } });
+    fireEvent.change(screen.getByLabelText(/Adresse mail/), { target: { value: 'johndoe@gmail.com' } });
+    fireEvent.change(screen.getByLabelText(/Mot de passe/), { target: { value: 'Jk0nl!56HJ' } });
+    fireEvent.change(screen.getByLabelText(/Date de naissance/), { target: { value: dayjs('01-01-2000') } });
+    fireEvent.change(screen.getByLabelText(/Code postal/), { target: { value: '10001' } });
+    fireEvent.change(screen.getByLabelText(/Ville/), {target: { value: "" },});
+  
+    expect(submitButton).toBeDisabled();
+  });
+
+  // Test de désactivation du bouton d'inscription si un champ est vide
+  test("Le bouton d'inscription est désactivé si le code postal est vide", () => {
+    render(<RegisterForm />);
+  
+    const submitButton = screen.getByRole("button", { name: /Inscription/ });
+    expect(submitButton).toBeDisabled();
+  
+    fireEvent.change(screen.getByLabelText(/Nom/), { target: { value: 'Doe' } });
+    fireEvent.change(screen.getByLabelText(/Prénom/), { target: { value: 'John' } });
+    fireEvent.change(screen.getByLabelText(/Adresse mail/), { target: { value: 'johndoe@gmail.com' } });
+    fireEvent.change(screen.getByLabelText(/Mot de passe/), { target: { value: 'Jk0nl!56HJ' } });
+    fireEvent.change(screen.getByLabelText(/Date de naissance/), { target: { value: dayjs('01-01-2000') } });
+    fireEvent.change(screen.getByLabelText(/Ville/), { target: { value: 'New York' } });
+    fireEvent.change(screen.getByLabelText(/Code postal/), {target: { value: "" },});
+  
+    expect(submitButton).toBeDisabled();
+  });
+
+  // Test d'attribution de valeur aux éléments du formulaire
   test('Attribution de valeur aux éléments du formulaire et vérification', async () => {
     render(<RegisterForm />);
 
@@ -50,17 +162,15 @@ describe('Tests d\'intégration pour le formulaire', () => {
     const lastNameInput = screen.getByLabelText(/Prénom/);
     const emailInput = screen.getByLabelText(/Adresse mail/);
     const passwordInpud = screen.getByLabelText(/Mot de passe/);
-    //const birthDateInput = screen.getByLabelText(/Date de naissance/);
+    const birthDateInput = screen.getByLabelText(/Date de naissance/);
     const cityInput = screen.getByLabelText(/Ville/);
     const postalCodeInput = screen.getByLabelText(/Code postal/);
-
-    //const date = dayjs('01-01-2000');
 
     fireEvent.change(firstNameInput, { target: { value: 'Doe' } });
     fireEvent.change(lastNameInput, { target: { value: 'John' } });
     fireEvent.change(emailInput, { target: { value: 'johndoe@gmail.com' } });
     fireEvent.change(passwordInpud, { target: { value: 'Jk0nl!56HJ' } });
-    //fireEvent.change(birthDateInput, { target: { value: '01-01-2000' } });
+    fireEvent.change(birthDateInput, { target: { value: dayjs('01-01-2000') } });
     fireEvent.change(cityInput, { target: { value: 'New York' } });
     fireEvent.change(postalCodeInput, { target: { value: '10001' } });
 
@@ -68,11 +178,12 @@ describe('Tests d\'intégration pour le formulaire', () => {
     expect(lastNameInput.value).toBe('John');
     expect(emailInput.value).toBe('johndoe@gmail.com');
     expect(passwordInpud.value).toBe('Jk0nl!56HJ');
-    //expect(birthDateInput.value).toBe('01-01-2000');
+    expect(birthDateInput.value).toBe(dayjs('01-01-2000'));
     expect(cityInput.value).toBe('New York');
     expect(postalCodeInput.value).toBe('10001');
   });
 
+  // Test de soumission du formulaire avec des données valides
   test('Soumet le formulaire avec des données valides', async () => {
     const handleSubmit = jest.fn();
     render(
@@ -111,6 +222,7 @@ describe('Tests d\'intégration pour le formulaire', () => {
     });
   });
 
+  // Test de soumission du formulaire avec des données valides et vérification du local storage
   test('Soumet le formulaire avec des données valides et vérifie le local storage', async () => {
     render(<RegisterForm />);
 
@@ -143,5 +255,34 @@ describe('Tests d\'intégration pour le formulaire', () => {
       city: 'New York',
       postalCode: '10001'
     });
+  });
+
+  // Test de soumission du formulaire avec des données invalides et vérification de l'affichage du toaster
+  test('Soumet le formulaire avec des données invalides et vérifie l\'affichage du toaster', async () => {
+    jest.mock('react-toastify', () => ({
+      toast: jest.fn()
+    }));
+    render(<RegisterForm />);
+
+    const firstNameInput = screen.getByLabelText(/Nom/);
+    const lastNameInput = screen.getByLabelText(/Prénom/);
+    const emailInput = screen.getByLabelText(/Adresse mail/);
+    const passwordInpud = screen.getByLabelText(/Mot de passe/);
+    const birthDateInput = screen.getByLabelText(/Date de naissance/);
+    const cityInput = screen.getByLabelText(/Ville/);
+    const postalCodeInput = screen.getByLabelText(/Code postal/);
+    const submitButton = screen.getByRole('button', { name: /Inscription/ });
+
+    fireEvent.change(firstNameInput, { target: { value: '123434' } });
+    fireEvent.change(lastNameInput, { target: { value: '65645' } });
+    fireEvent.change(emailInput, { target: { value: 'johndoe@' } });
+    fireEvent.change(passwordInpud, { target: { value: 'happy' } });
+    fireEvent.change(birthDateInput, { target: { value: dayjs('01-01-2015') } });
+    fireEvent.change(cityInput, { target: { value: '7888' } });
+    fireEvent.change(postalCodeInput, { target: { value: '99' } });
+
+    fireEvent.click(submitButton);
+
+    expect(toast).toHaveBeenCalledWith('Le formulaire contient des erreurs. Veuillez vérifier les champs.');
   });
 });
