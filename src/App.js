6ded286c34aada285cadf4a55cb671beb40ed04e
Elelia <llucas@ensup.eu>
2024-03-07 15:01:07 +0100

    update

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import RegisterForm from './components/RegisterForm';
 import axios from 'axios';
 import { useState, useEffect } from 'react';
+require("dotenv").config();
 
 export default function App() {
   const port = process.env.PORT || 8000;
